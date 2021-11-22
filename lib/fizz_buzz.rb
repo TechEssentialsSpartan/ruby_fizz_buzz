bc10863280d766392c8168ac284645bf594339e2
Shun712 <sh705vi@gmail.com>
2021-11-22 22:39:00 +0900

    fizz_buzzの実装

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,3 +1,11 @@
 def fizz_buzz(number)
-  # 実装してください
+  if number % 15 == 0
+    return "Fizz Buzz"
+  elsif number % 3 == 0
+    return "Fizz"
+  elsif number % 5 == 0
+    return "Buzz"
+  else
+    return "#{number}"
+  end
 end
