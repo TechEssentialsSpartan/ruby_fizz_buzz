e5585677a5cd39a5f23516670377fb00a1841509
suna-g <sunagawa69852@gmail.com>
2022-01-15 08:43:21 +0900

    fizzbuzzテスト

--- test/fizz_buzz_test.rb
+++ test/fizz_buzz_test.rb
@@ -9,7 +9,7 @@ class FizzBuzzTest < Minitest::Test
     assert_equal '4', fizz_buzz(4)
     assert_equal 'Buzz', fizz_buzz(5)
     assert_equal 'Fizz', fizz_buzz(6)
-    assert_equal 'Fizz Buzz', fizz_buzz(15)
-    assert_equal 'Fizz Buzz', fizz_buzz(150)
+    assert_equal 'Fizz_Buzz', fizz_buzz(15)
+    assert_equal 'Fizz_Buzz', fizz_buzz(150)
   end
 end
