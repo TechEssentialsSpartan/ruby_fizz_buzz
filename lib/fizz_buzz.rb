2603feaadb22472b2950877e2b70559e67587a81
An39nA0219 <an39na10282004@yahoo.co.jp>
2022-02-27 23:59:53 +0900

    fizzbuzzの実装

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,3 +1,14 @@
 def fizz_buzz(number)
   # 実装してください
+  # 3の倍数の時はFizz、5の倍数の時はBuzz、3と5の公倍数の時はFizzBuzzを返すプログラム
+
+  if number % 3 == 0 && number % 5 == 0
+    'Fizz Buzz'
+  elsif number % 3 == 0
+    'Fizz'
+  elsif number % 5 == 0
+    'Buzz'
+  else
+    number.to_s
+  end
 end
