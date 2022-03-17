12dc54f7caceb250d08cda2d1b281b3db7fb5a3f
gyara658 <gyara658@gmail.com>
2022-03-18 01:25:02 +0900

    fizzbuzz

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,3 +1,13 @@
 def fizz_buzz(number)
-  # 実装してください
+  num = number.to_i
+
+  if num % 15 == 0
+    'Fizz Buzz'
+  elsif num % 5 == 0
+    'Buzz'
+  elsif num % 3 == 0
+    'Fizz'
+  else
+    num.to_s
+  end
 end
