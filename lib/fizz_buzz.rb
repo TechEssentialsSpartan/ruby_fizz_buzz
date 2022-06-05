2d67663e79243e29b74176c760e0323f79ed9a2b
mai-kobayashi23 <mai0323.k@gmail.com>
2022-06-05 16:33:23 +0900

    FizzBuzz実装完了

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,11 +1,11 @@
 def fizz_buzz(number)
   if number % 15 == 0
     'Fizz Buzz'
-    if number % 3 == 0
-      'Fizz'
-    if number % 5 == 0
-      'Buzz'
-    else
-      number.to_s
+  elsif number % 3 == 0
+    'Fizz'
+  elsif number % 5 == 0
+    'Buzz'
+  else
+    number.to_s
   end
 end
