23f884172f8a60d5fad4d7b584323573d8476548
Kei <1372012@g.iuhw.ac.jp>
2022-08-21 20:01:42 +0900

    Update fizz_buzz.rb

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,3 +1,11 @@
 def fizz_buzz(number)
-  # 実装してください
+  if number % 15 == 0
+    'FizzBuzz'
+    elsif number % 3 == 0
+    'Fizz'
+    elsif number % 5 == 0
+    'Buzz'
+    else
+    n.to_s
+    end
 end
