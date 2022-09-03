735a3942cd8c8559aba76691d24343ae9c2e0e19
tanamaru <yt.0408.mmm@gmail.com>
2022-09-03 17:23:03 +0900

    実装

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,3 +1,14 @@
 def fizz_buzz(number)
   # 実装してください
+  if number % 15 == 0
+    puts "FizzBuzz"
+  elsif number % 3 == 0
+    puts "Fizz"
+  elsif number % 5 == 0
+    puts "Buzz"
+  else 
+    puts number
+  end
 end
+
+fizz_buzz(15)
